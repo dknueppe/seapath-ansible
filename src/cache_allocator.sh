54c3fee4badbb96df20366af9a233fbcdaacd24a
Daniel Knüppe <daniel_k@posteo.net>
2024-06-27 14:03:10 +0200

    added welotec recommended tuned profile and modified hw setup in scripts

--- src/cache_allocator.sh
+++ src/cache_allocator.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-# setup Cache-Way masks for COS 0-4
+# setup Cache-Way masks for COS 0-3
 wrmsr 0xc90 0xe00 # 1110 0000 0000
 wrmsr 0xc91 0x1fc # 0001 1100 0000
 wrmsr 0xc92 0x038 # 0000 0011 1000
 wrmsr 0xc93 0x007 # 0000 0000 0111
 
-# cores 0-2 remain in default COS 0
-# is equivalent to the following
-#pqos -a 'core:1=3-5'
-#pqos -a 'core:2=6'
-#pqos -a 'core:3=7'
-#
-# set core 3-5 to use COS 1
+# cores 0 and 8 remain in default COS 0
+# set core 1-3,9-11 to use COS 1
+wrmsr -p 1 0xc8f 0x100000000
+wrmsr -p 2 0xc8f 0x100000000
 wrmsr -p 3 0xc8f 0x100000000
-wrmsr -p 4 0xc8f 0x100000000
-wrmsr -p 5 0xc8f 0x100000000
-# set core 6 to use COS 2
-wrmsr -p 6 0xc8f 0x200000000
-# set core 7 to use COS 3
+wrmsr -p 9 0xc8f 0x100000000
+wrmsr -p 10 0xc8f 0x100000000
+wrmsr -p 11 0xc8f 0x100000000
+# set core 4-5 to use COS 2
+wrmsr -p 4 0xc8f 0x200000000
+wrmsr -p 5 0xc8f 0x200000000
+# set core 6-7 to use COS 3
+wrmsr -p 6 0xc8f 0x300000000
 wrmsr -p 7 0xc8f 0x300000000
+
+# set GPU mask to the same as COS 0
+wrmsr 0x18b0 0xe00
+wrmsr 0x18b1 0xe00
+wrmsr 0x18b2 0xe00
+wrmsr 0x18b3 0xe00
+
+# set WRC mask to the same as COS 0
+wrmsr 0x18d0 0xe00
+wrmsr 0x18d1 0xe00
+wrmsr 0x18d2 0xe00
+wrmsr 0x18d3 0xe00
 
--- src/online_cpus.sh
+++ src/online_cpus.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-for cpu_online in /sys/devices/system/cpu/cpu*/online
-do
-	echo 1 > $cpu_online
-done
+echo 0 > /sys/devices/system/cpu/cpu12/online
+echo 0 > /sys/devices/system/cpu/cpu13/online
+echo 0 > /sys/devices/system/cpu/cpu14/online
+echo 0 > /sys/devices/system/cpu/cpu15/online
+
+#echo 1500000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 2000000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 2000000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 2000000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+#echo 1500000 > /sys/devices/system/cpu/cpu8/cpufreq/scaling_max_freq
+echo 1500000 > /sys/devices/system/cpu/cpu9/cpufreq/scaling_max_freq
+echo 1500000 > /sys/devices/system/cpu/cpu10/cpufreq/scaling_max_freq
+echo 1500000 > /sys/devices/system/cpu/cpu11/cpufreq/scaling_max_freq
+
+echo 2900000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq
+echo 2000000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq
+echo 2900000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_max_freq
+echo 2000000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_max_freq
+
+echo 2900000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+echo 2000000 > /sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq
+echo 2900000 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
+echo 2000000 > /sys/devices/system/cpu/cpu7/cpufreq/scaling_min_freq
